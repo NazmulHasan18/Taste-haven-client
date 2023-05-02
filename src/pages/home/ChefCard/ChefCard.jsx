2d391c220705564c8876ccf07d2e7021c78c9e71
NazmulHasan18 <nazmul182218@gmail.com>
2023-05-02 13:50:35 +0600

    Chef Card Section Modified

--- src/pages/home/ChefCard/ChefCard.jsx
+++ src/pages/home/ChefCard/ChefCard.jsx
@@ -1,11 +1,42 @@
+/* eslint-disable react/prop-types */
 // eslint-disable-next-line no-unused-vars
 import React from "react";
+import { Button, Card } from "react-bootstrap";
+import { FaHeart } from "react-icons/fa";
+import { BiBowlHot } from "react-icons/bi";
 
-const ChefCard = () => {
+const ChefCard = ({ chef }) => {
+   const { name, image, experience, foodItems, likes, chefPosition } = chef;
+   console.log(chef);
    return (
-      <div>
-         <h2>This is chef card</h2>
-      </div>
+      <Card className="mb-5 border-danger " style={{ width: "18rem", height: "32rem" }} bg="warning">
+         <Card.Img variant="top" src={image} style={{ height: "19rem" }} />
+         <Card.Body>
+            <Card.Title>
+               <h3>{name}</h3>
+            </Card.Title>
+            <Card.Text className="mb-1">
+               <span className="fw-semibold">Experience :</span> {experience}
+            </Card.Text>
+            <Card.Text className="mb-1">
+               <span className="fw-semibold">Position :</span> {chefPosition}
+            </Card.Text>
+            <div className="d-flex justify-content-between">
+               <Card.Text className="mb-1">
+                  <span className="fw-semibold">Recipes :</span> {foodItems.length}
+               </Card.Text>
+               <Card.Text className="mb-1">
+                  <span className="fw-semibold">
+                     <FaHeart className="text-danger"></FaHeart>
+                  </span>{" "}
+                  {likes}
+               </Card.Text>
+            </div>
+            <Button variant="danger" className="mt-3">
+               View Recipes <BiBowlHot></BiBowlHot>
+            </Button>
+         </Card.Body>
+      </Card>
    );
 };
 
--- src/pages/home/Chefs/Chefs.jsx
+++ src/pages/home/Chefs/Chefs.jsx
@@ -1,14 +1,60 @@
 // eslint-disable-next-line no-unused-vars
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
+// Import Swiper React components
+import { Swiper, SwiperSlide } from "swiper/react";
+
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+import { Navigation, Pagination } from "swiper";
+
 import ChefCard from "../ChefCard/ChefCard";
 
 const Chefs = () => {
+   const [chefs, setChefs] = useState([]);
+   useEffect(() => {
+      fetch("http://localhost:3000/chefs")
+         .then((res) => res.json())
+         .then((data) => setChefs(data));
+   }, []);
    return (
-      <div>
-         <h2>Chef will display Hare</h2>
-         <div>
-            <ChefCard></ChefCard>
-         </div>
+      <div className="my-5">
+         <h2 className="text-center py-5">The Hand You Like To EAT (Chefs)</h2>
+         <Swiper
+            slidesPerView={3}
+            navigation={true}
+            spaceBetween={30}
+            centeredSlides={true}
+            loop={true}
+            pagination={{
+               clickable: true,
+            }}
+            breakpoints={{
+               640: {
+                  slidesPerView: 2,
+                  spaceBetween: 20,
+               },
+               768: {
+                  slidesPerView: 3,
+                  spaceBetween: 40,
+               },
+               1024: {
+                  slidesPerView: 3.5,
+                  spaceBetween: 50,
+               },
+            }}
+            modules={[Navigation, Pagination]}
+            className="mySwiper"
+         >
+            {chefs.map((chef) => {
+               return (
+                  <SwiperSlide key={chef.id}>
+                     <ChefCard chef={chef}></ChefCard>
+                  </SwiperSlide>
+               );
+            })}
+         </Swiper>
       </div>
    );
 };
